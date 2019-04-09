d74d4d94d23bc27f672334c60494600cda2ac7d7
Martin Kearn <martin.kearn@microsoft.com>
2019-04-09 13:56:03 +0100

    Refactored to GetString

--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -42,7 +42,7 @@ namespace StarterBot.Dialogs
 
         private async Task<DialogTurnResult> SayHiAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            await stepContext.Context.SendActivityAsync($"{await _strings.Get("rootdialogwelcome", null)}", cancellationToken: cancellationToken);
+            await stepContext.Context.SendActivityAsync($"{await _strings.GetString("rootdialogwelcome", null)}", cancellationToken: cancellationToken);
 
             return await stepContext.NextAsync(cancellationToken: cancellationToken);
         }
@@ -53,8 +53,8 @@ namespace StarterBot.Dialogs
                 new PromptOptions
                 {
                     Choices = ChoiceFactory.ToChoices(new List<string> { ChoicePromptDialogA, ChoicePromptDialogB }),
-                    Prompt = MessageFactory.Text(await _strings.Get("whichflow", null)),
-                    RetryPrompt = MessageFactory.Text(await _strings.Get("invalidresponse", null))
+                    Prompt = MessageFactory.Text(await _strings.GetString("whichflow", null)),
+                    RetryPrompt = MessageFactory.Text(await _strings.GetString("invalidresponse", null))
 
                 },
                 cancellationToken).ConfigureAwait(false);
