6e866cac2ac53b2df4c1dfa1115e876716110972
Martin Simecek <msimecek@users.noreply.github.com>
2019-04-09 13:45:46 +0100

    Tokens to Strings

--- Services/Strings.cs
+++ Services/Strings.cs
@@ -14,14 +14,14 @@ namespace StarterBot.Services
         {
             _strings = new Dictionary<string, string>
             {
-                { "welcome", "Welcome to the bot" },
+                { "welcome", "Welcome to the bot, {0}" },
                 { "foo", "foo" }
             };
         }
 
-        public async Task<string> GetString(string key, string language)
+        public async Task<string> GetString(string key, string language, params string[] tokens)
         {
-            return await Task.FromResult(_strings[key.ToLower()]);            
+            return await Task.FromResult(string.Format(_strings[key.ToLower()], tokens));            
         }
     }
 }
