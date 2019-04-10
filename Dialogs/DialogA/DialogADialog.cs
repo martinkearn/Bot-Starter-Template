20e831d7029cdeed48d5e70a615111ea3c2b0669
Martin Kearn <martin.kearn@microsoft.com>
2019-04-10 14:56:44 +0100

    Seperated into more granular waterfall steps

--- Dialogs/DialogA/DialogADialog.cs
+++ Dialogs/DialogA/DialogADialog.cs
@@ -21,8 +21,11 @@ namespace StarterBot.Dialogs.DialogA
             // Add waterfall dialog steps
             var waterfallSteps = new WaterfallStep[]
             {
-                SayHiAskNameAsync,
+                SayHiAsync,
+                AskNameAsync,
+                HandleNameAsync,
                 AskAgeAsync,
+                HandleAgeAsync,
                 EndAsync,
             };
             AddDialog(new WaterfallDialog(InitialDialogId, waterfallSteps));
@@ -32,35 +35,51 @@ namespace StarterBot.Dialogs.DialogA
             AddDialog(new NumberPrompt<int>("agePrompt"));
         }
 
-        private async Task<DialogTurnResult> SayHiAskNameAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> SayHiAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             await stepContext.Context.SendActivityAsync($"{DialogAStrings.Welcome}", cancellationToken: cancellationToken);
 
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> AskNameAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
             // Get the state object from user state.
             var state = await _globalUserStateAccessor.GetAsync(stepContext.Context, () => new GlobalUserState());
 
             //Check if user already provided country in DialogB and modify messages with provided info
-            var msg = (string.IsNullOrEmpty(state.Country)) ? "What's your name?" : $"You already provided your country: {state.Country}, what's your name?";
+            var msg = (string.IsNullOrEmpty(state.Country)) ? 
+                "What's your name?" : 
+                $"You already provided your country: {state.Country}, what's your name?";
 
             return await stepContext.PromptAsync("namePrompt", new PromptOptions { Prompt = MessageFactory.Text(msg) }, cancellationToken);
         }
 
-        private async Task<DialogTurnResult> AskAgeAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        private async Task<DialogTurnResult> HandleNameAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
             stepContext.Values["name"] = (string)stepContext.Result;
 
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> AskAgeAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
             return await stepContext.PromptAsync("agePrompt", new PromptOptions { Prompt = MessageFactory.Text(string.Format($"What's your age {stepContext.Values["name"]}?")) }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> HandleAgeAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            stepContext.Values["age"] = (int)stepContext.Result;
+
+            return await stepContext.NextAsync(cancellationToken: cancellationToken);
         }
 
         private async Task<DialogTurnResult> EndAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
-            stepContext.Values["age"] = (int)stepContext.Result;
-
-            // Get the current profile object from user state.
+            // Update state from stepContent
             var state = await _globalUserStateAccessor.GetAsync(stepContext.Context, () => new GlobalUserState());
             state.Name = (string)stepContext.Values["name"];
             state.Age = (int)stepContext.Values["age"];
-
 
             //Check if user already provided country in DialogB and modify messages with provided info
             var msg = (string.IsNullOrEmpty(state.Country)) ? $"Thank you {state.Name} for providing your age {state.Age}." : $"Thank you {state.Name}, {state.Age} from {state.Country} for providing your information.";
