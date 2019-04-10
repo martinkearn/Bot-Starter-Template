94b758d5c7f6cb862503fcd8f00d2f9505834c5f
Ibrahim KIVANC <ikivanc@hotmail.com>
2019-04-10 14:07:02 +0300

    UserProfile userstate is added
    UserProfile userstate is added

---
+++ Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterBot.Models
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+    }
+}
